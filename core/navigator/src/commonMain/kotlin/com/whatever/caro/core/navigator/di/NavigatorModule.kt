f0fec1b4042d9a1ace04703722fceba792500059
함건형 <ham121985@gmail.com>
2026-01-26 16:57:31 +0900

    chore : 사용 않는 import 구문 제거

--- core/navigator/src/commonMain/kotlin/com/whatever/caro/core/navigator/di/NavigatorModule.kt
+++ core/navigator/src/commonMain/kotlin/com/whatever/caro/core/navigator/di/NavigatorModule.kt
@@ -1,12 +1,7 @@
 package com.whatever.caro.core.navigator.di
 
-import com.whatever.caro.core.navigator.dispatcher.NavigationDispatcher
-import com.whatever.caro.core.navigator.dispatcher.NavigationDispatcherImpl
 import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Configuration
-import org.koin.core.annotation.Factory
 import org.koin.core.annotation.Module
-import org.koin.core.annotation.Single
 
 @Module
 @ComponentScan("com.whatever.caro.core.navigator")
