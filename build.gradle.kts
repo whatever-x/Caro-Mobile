c2de88efb750857e4a09d254f718c5f33f9434db
함건형 <ham121985@gmail.com>
2026-02-03 15:49:20 +0900

    build : spotless 린트 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.spotless.extra.wtp.EclipseWtpFormatterStep
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
@@ -13,4 +16,39 @@ plugins {
     alias(libs.plugins.mokkery) apply false
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.kotest) apply false
+}
+
+val ktlintCliVersion = libs.versions.ktlint.cli.get()
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+
+    configure<SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude(
+                "**/build/**",
+                "**/generated/**"
+            )
+            ktlint(ktlintCliVersion)
+        }
+
+        kotlinGradle {
+            target("**/*.gradle.kts")
+            ktlint(ktlintCliVersion)
+        }
+
+        format("xml") {
+            target(
+                "src/**/values/*.xml",
+                "src/**/AndroidManifest.xml"
+            )
+            targetExclude(
+                "**/build/**",
+                "**/generated/**"
+            )
+
+            eclipseWtp(EclipseWtpFormatterStep.XML)
+        }
+    }
 }
