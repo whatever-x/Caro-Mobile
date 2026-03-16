125814462514804f0682d74d8cf3acacd4fef6bf
swyu <swyu@raoncorp.com>
2026-03-16 18:58:57 +0900

    feat : Android 구글 로그인 구현
    # Conflicts:
    #	feature/login/src/commonMain/kotlin/com/whatever/caro/feature/login/LoginViewModel.kt

---
+++ core/model/src/commonMain/kotlin/com/whatever/caro/core/model/auth/SocialLoginType.kt
@@ -0,0 +1,7 @@
+package com.whatever.caro.core.model.auth
+
+enum class SocialLoginType (val value : String){
+    GOOGLE("google"),
+    APPLE("apple"),
+    ;
+}
--- feature/login/src/commonMain/kotlin/com/whatever/caro/feature/login/LoginViewModel.kt
+++ feature/login/src/commonMain/kotlin/com/whatever/caro/feature/login/LoginViewModel.kt
@@ -1,21 +1,45 @@
 package com.whatever.caro.feature.login
 
 import com.whatever.caro.core.viewmodel.BaseViewModel
+import com.whatever.caro.feature.login.model.AppleUser
+import com.whatever.caro.feature.login.model.GoogleUser
+import com.whatever.caro.feature.login.model.LoginError
+import com.whatever.caro.feature.login.model.SocialLoginResult
 import com.whatever.caro.feature.login.mvi.LoginIntent
 import com.whatever.caro.feature.login.mvi.LoginSideEffect
 import com.whatever.caro.feature.login.mvi.LoginState
 
-class LoginViewModel :
-    BaseViewModel<LoginState, LoginIntent, LoginSideEffect>(
-        initialState = LoginState(),
-    ) {
+class LoginViewModel : BaseViewModel<LoginState, LoginIntent, LoginSideEffect>(
+    initialState = LoginState(),
+) {
     override fun handleClientException(throwable: Throwable) {
         TODO()
     }
 
     override suspend fun handleIntent(intent: LoginIntent) {
         when (intent) {
-            is LoginIntent.ClickLogin -> postSideEffect(LoginSideEffect.NavigateHome)
+            is LoginIntent.ClickGoogleLoginButton -> loginWithGoogle(intent)
+            is LoginIntent.ClickAppleLoginButton -> loginWithApple(intent)
+        }
+    }
+
+    private fun loginWithGoogle(
+        intent: LoginIntent.ClickGoogleLoginButton
+    ) {
+        when (intent.result) {
+            SocialLoginResult.Failed -> postSideEffect(LoginSideEffect.ShowErrorToast(error = LoginError.UNKNOWN))
+            is SocialLoginResult.Success<GoogleUser> -> TODO()
+            SocialLoginResult.UserCancelled -> postSideEffect(LoginSideEffect.ShowErrorToast(error = LoginError.USER_CANCELLED))
+        }
+    }
+
+    private fun loginWithApple(
+        intent: LoginIntent.ClickAppleLoginButton
+    ) {
+        when (intent.result) {
+            SocialLoginResult.Failed -> postSideEffect(LoginSideEffect.ShowErrorToast(error = LoginError.UNKNOWN))
+            is SocialLoginResult.Success<AppleUser> -> TODO()
+            SocialLoginResult.UserCancelled -> postSideEffect(LoginSideEffect.ShowErrorToast(error = LoginError.USER_CANCELLED))
         }
     }
 }
