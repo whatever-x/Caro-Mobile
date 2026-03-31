89021dfa76c0abe2dde73e62f16e07fa703a1a01
RyuSw-cs <rsw1452@naver.com>
2026-04-01 01:40:41 +0900

    chore : 사용하지 않는 모듈 제거

--- composeApp/src/commonMain/kotlin/com/whatever/caro/composeApp/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caro/composeApp/di/Koin.kt
@@ -3,13 +3,13 @@ package com.whatever.caro.composeApp.di
 import org.koin.core.annotation.KoinApplication
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.includes
-import org.koin.ksp.generated.startKoin
+import org.koin.plugin.module.dsl.startKoin
 
 @KoinApplication
 object KoinApp
 
 fun initKoin(configuration: KoinAppDeclaration? = null) {
-    KoinApp.startKoin {
+    startKoin<KoinApp> {
         includes(configuration)
         modules(
             navigationModule,
