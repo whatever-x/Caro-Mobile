93f4677fbb97c5f0a1f6443c50e5af0cbfbc560e
evergreentree97 <tkdfhr7497@naver.com>
2026-04-01 19:44:31 +0900

    feat : 프로필 모듈 생성하고, 닉네임 생성 관련 Navigation 세팅

---
+++ feature/profile/src/commonMain/kotlin/com/whatever/caro/feature/profile/di/ProfileModule.kt
@@ -0,0 +1,8 @@
+package com.whatever.caro.feature.profile.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.whatever.caro.feature.profile")
+class ProfileModule
