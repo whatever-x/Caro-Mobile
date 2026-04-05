6897dc61fd85611cb98d64d769bf75dc8cb27ecb
RyuSw-cs <rsw1452@naver.com>
2026-04-05 23:40:50 +0900

    refactor : loginModule에서 socialModule 분리

--- feature/login/src/iosMain/kotlin/com/whatever/caro/feature/login/di/LoginModule.ios.kt
+++ feature/login/src/iosMain/kotlin/com/whatever/caro/feature/login/di/LoginModule.ios.kt
@@ -4,10 +4,12 @@ import com.whatever.caro.feature.login.provider.AppleAuthProvider
 import com.whatever.caro.feature.login.provider.AppleAuthProviderImpl
 import com.whatever.caro.feature.login.provider.GoogleAuthProvider
 import com.whatever.caro.feature.login.provider.GoogleAuthProviderImpl
+import kotlinx.cinterop.ExperimentalForeignApi
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-actual val loginModule: Module =
+@OptIn(ExperimentalForeignApi::class)
+actual val socialModule: Module =
     module {
         factory<GoogleAuthProvider> { GoogleAuthProviderImpl() }
         factory<AppleAuthProvider> { AppleAuthProviderImpl() }
