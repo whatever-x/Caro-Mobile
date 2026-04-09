ca9e059e80deb6c133513ed1975ec9249ee45d75
RyuSw-cs <rsw1452@naver.com>
2026-04-09 23:53:09 +0900

    chore : 중복 모듈 수정

--- feature/login/src/commonMain/kotlin/com/whatever/caro/feature/login/di/Modules.kt
+++
@@ -1,5 +0,0 @@
-package com.whatever.caro.feature.login.di
-
-import org.koin.core.module.Module
-
-expect val loginModule: Module
--- feature/login/src/commonMain/kotlin/com/whatever/caro/feature/login/model/LoginError.kt
+++ feature/login/src/commonMain/kotlin/com/whatever/caro/feature/login/model/LoginError.kt
@@ -1,11 +1,5 @@
 package com.whatever.caro.feature.login.model
 
-import androidx.compose.runtime.Composable
-import caromobile.feature.login.generated.resources.Res
-import caromobile.feature.login.generated.resources.login_cancelled
-import caromobile.feature.login.generated.resources.login_error
-import org.jetbrains.compose.resources.stringResource
-
 enum class LoginError {
     UNKNOWN,
     USER_CANCELLED,
