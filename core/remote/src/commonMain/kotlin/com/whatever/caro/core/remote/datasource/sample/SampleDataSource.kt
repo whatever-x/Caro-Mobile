8401ce907af141d40b5edf3c35a4e9b7bfcda692
함건형 <ham121985@gmail.com>
2026-04-15 19:23:46 +0900

    feat : SampleDataSource 구현

---
+++ core/remote/src/commonMain/kotlin/com/whatever/caro/core/remote/datasource/sample/SampleDataSource.kt
@@ -0,0 +1,7 @@
+package com.whatever.caro.core.remote.datasource.sample
+
+interface SampleDataSource {
+
+    suspend fun getString(): String
+
+}
--- core/remote/src/commonMain/kotlin/com/whatever/caro/core/remote/di/RemoteModule.kt
+++ core/remote/src/commonMain/kotlin/com/whatever/caro/core/remote/di/RemoteModule.kt
@@ -2,6 +2,8 @@ package com.whatever.caro.core.remote.di
 
 import com.whatever.caro.core.remote.datasource.demo.DemoDataSource
 import com.whatever.caro.core.remote.datasource.demo.DemoDataSourceImpl
+import com.whatever.caro.core.remote.datasource.sample.SampleDataSource
+import com.whatever.caro.core.remote.datasource.sample.SampleDataSourceImpl
 import com.whatever.caro.core.remote.di.qualifier.NetworkClient
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
@@ -14,4 +16,11 @@ val remoteModule =
                 defaultClient = get(named(NetworkClient.DEFAULT)),
             )
         }
+
+        single<SampleDataSource> {
+            SampleDataSourceImpl(
+                httpClient = get(named(NetworkClient.DEFAULT))
+            )
+        }
     }
+
