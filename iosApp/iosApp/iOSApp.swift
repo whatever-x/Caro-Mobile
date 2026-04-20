dabf0ad24f384e83457a7994ea43445e00778a71
함건형 <ham121985@gmail.com>
2026-04-20 20:49:50 +0900

    feat : iOS - Initializer (FirebaseApp, Firebase Analytics, Firebase Crashlytics, Napier) 구현

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 import ComposeApp
 import GoogleSignIn
+import FirebaseCore
+import FirebaseAnalytics
+import FirebaseCrashlytics
 
 @main
 struct iOSApp: App {
     
     init() {
+        FirebaseApp.configure()
+        Analytics.setAnalyticsCollectionEnabled(true)
+        Crashlytics.crashlytics().setCrashlyticsCollectionEnabled(true)
         KoinKt.doInitKoin()
+        NapierInitializerKt.doInitNapier()
     }
     
     var body: some Scene {
