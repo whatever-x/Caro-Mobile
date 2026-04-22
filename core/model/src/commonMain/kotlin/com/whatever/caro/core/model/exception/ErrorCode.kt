dbfd9d93bac1c18349e381e712dbd9f6efb2cd7a
함건형 <ham121985@gmail.com>
2026-04-23 00:18:00 +0900

    feat : 예외코드 INVALID_RESPONSE, NETWORK_002 추가

--- core/model/src/commonMain/kotlin/com/whatever/caro/core/model/exception/ErrorCode.kt
+++ core/model/src/commonMain/kotlin/com/whatever/caro/core/model/exception/ErrorCode.kt
@@ -3,5 +3,8 @@ package com.whatever.caro.core.model.exception
 object ErrorCode {
     const val UNKNOWN_001 = "UNKNOWN_001"
 
+    const val INVALID_RESPONSE = "INVALID_RESPONSE"
     const val NETWORK_001 = "NETWORK_001"
+    const val NETWORK_002 = "NETWORK_002"
+
 }
