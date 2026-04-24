c44561688bd9f631cb2f91d52b573e2de7f82907
함건형 <ham121985@gmail.com>
2026-04-24 17:47:35 +0900

    refactor : 파이어베이스 수집 분기 수정

--- androidApp/src/main/kotlin/com/whatever/caro/app/initializer/FirebaseAnalyticsInitializer.kt
+++ androidApp/src/main/kotlin/com/whatever/caro/app/initializer/FirebaseAnalyticsInitializer.kt
@@ -3,12 +3,13 @@ package com.whatever.caro.app.initializer
 import android.content.Context
 import androidx.startup.Initializer
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.whatever.caro.app.BuildConfig
 import io.github.aakira.napier.Napier
 
 class FirebaseAnalyticsInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
-        FirebaseAnalytics.getInstance(context).setAnalyticsCollectionEnabled(true)
+        FirebaseAnalytics.getInstance(context).setAnalyticsCollectionEnabled(!BuildConfig.DEBUG)
 
         Napier.d("Firebase Analytics 초기화")
     }
--- androidApp/src/main/kotlin/com/whatever/caro/app/initializer/FirebaseCrashlyticsInitializer.kt
+++ androidApp/src/main/kotlin/com/whatever/caro/app/initializer/FirebaseCrashlyticsInitializer.kt
@@ -3,12 +3,13 @@ package com.whatever.caro.app.initializer
 import android.content.Context
 import androidx.startup.Initializer
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.whatever.caro.app.BuildConfig
 import io.github.aakira.napier.Napier
 
 class FirebaseCrashlyticsInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
-        FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = true
+        FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = !BuildConfig.DEBUG
 
         Napier.d("Firebase Crashlytics 초기화")
     }
