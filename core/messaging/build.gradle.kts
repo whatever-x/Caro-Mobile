13ce6e345957067d2615e0758ee1e0a0ea9c887b
swyu <swyu@raoncorp.com>
2026-05-12 17:11:29 +0900

    refactor : FCM 관련 코틀린 코드로 변경
    # Conflicts:
    #	core/messaging/src/iosMain/kotlin/com/whatever/caro/core/messaging/di/Modules.ios.kt

--- core/messaging/build.gradle.kts
+++ core/messaging/build.gradle.kts
@@ -1,12 +1,36 @@
+import io.github.frankois944.spmForKmp.swiftPackageConfig
+import java.net.URI
+
 plugins {
     id("caro.kmp")
     id("caro.kmp.android")
     id("caro.kmp.ios")
     id("caro.koin")
     id("caro.kmp.test")
+    alias(libs.plugins.kmp.spm)
 }
 
 kotlin {
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach { iosTarget ->
+        iosTarget.swiftPackageConfig {
+            minIos = "15.0"
+
+            dependency {
+                remotePackageVersion(
+                    url = URI("https://github.com/firebase/firebase-ios-sdk"),
+                    version = "12.12.1",
+                    products = {
+                        add("FirebaseMessaging", exportToKotlin = true)
+                    },
+                )
+            }
+        }
+    }
+
     android {
         namespace = "com.whatever.caro.core.messaging"
     }
