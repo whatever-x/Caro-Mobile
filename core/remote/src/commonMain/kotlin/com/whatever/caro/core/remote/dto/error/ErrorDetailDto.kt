691aa763d102d0fca26ed0b797ad2f33aab5dd27
swyu <swyu@raoncorp.com>
2026-05-22 15:52:38 +0900

    fix: ErrorDetailDto 구조 변경

--- core/remote/src/commonMain/kotlin/com/whatever/caro/core/remote/dto/error/ErrorDetailDto.kt
+++ core/remote/src/commonMain/kotlin/com/whatever/caro/core/remote/dto/error/ErrorDetailDto.kt
@@ -6,8 +6,6 @@ import kotlinx.serialization.Serializable
 data class ErrorDetailDto(
     val code: String,
     val message: String,
-    val debugMessage: String? = null,
-    val description: String? = null,
-    val traceId: String? = null,
-    val fieldErrors: List<FieldErrorDto>? = null,
+    val traceId: String?,
+    val fieldErrors: List<FieldErrorDto>?,
 )
