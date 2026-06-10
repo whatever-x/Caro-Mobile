841aa06c782b2d825980303dd3c7d5755db5be27
swyu <swyu@raoncorp.com>
2026-06-10 17:14:51 +0900

    chore: 클래스명 수정

--- core/remote/src/commonMain/kotlin/com/whatever/caro/core/remote/dto/cardController/request/CreateCardItemDto.kt
+++ core/remote/src/commonMain/kotlin/com/whatever/caro/core/remote/dto/cardController/request/CreateCardItemDto.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 // AUTO-GENERATED FROM SWAGGER — 직접 수정하지 마세요
 @Serializable
-data class CreateCardItem(
+data class CreateCardItemDto(
     /** 카드 필드 */
     val fields: Map<String, String>,
     /** 카드 타입 */
