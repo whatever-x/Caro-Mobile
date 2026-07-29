de2f7d7b19e558b421d29618ce324855cbe3d896
함건형 <ham121985@gmail.com>
2026-07-29 22:08:40 +0900

    chore: CTA 버튼 테스트 DSL 컨벤션 적용

--- feature/profile/src/commonTest/kotlin/ProfileCtaButtonTest.kt
+++ feature/profile/src/commonTest/kotlin/ProfileCtaButtonTest.kt
@@ -4,9 +4,10 @@ import androidx.compose.ui.layout.ContentScale
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
-class ProfileCtaButtonTest :
-    FunSpec({
+class ProfileCtaButtonTest : FunSpec() {
+    init {
         test("CTA 로딩 로띠를 버튼 텍스트 영역에 맞게 확대한다") {
             ProfileButtonLoadingContentScale shouldBe ContentScale.Crop
         }
-    })
+    }
+}
